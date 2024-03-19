c0b5102123e3709291049d8f0f867a05de310a78
Iane14093051 <87408494+Iane14093051@users.noreply.github.com>
2024-03-19 16:49:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.post("/newad", (req, res) => {
 	var data = new ad(req.body);
 	adminverify(token).then((flag) => {
 		if(flag == 1){
-			data.save().then((docs) => {
+			data.tosave(res).then((docs) => {
 				
 				return res.status(200).send(docs);
 			}).catch(err => {
